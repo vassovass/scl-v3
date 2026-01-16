5aefc0286c41fc9f779a768f15a7714cc8bbe6b9
vassovass <vasso@vaseo.co.za>
2026-01-16 23:12:29 +0700

    refactor(analytics): export PostHog components from barrel file

--- src/components/analytics/index.ts
+++ src/components/analytics/index.ts
@@ -1,7 +1,9 @@
 /**
  * Analytics components barrel export
  * @see PRD 14: Analytics GTM & GA4
+ * @see PostHogProvider.tsx for PostHog SDK integration
  */
 
 export { GoogleTagManager, GoogleTagManagerNoscript } from './GoogleTagManager';
 export { CookieConsentBanner } from './CookieConsent';
+export { PostHogProvider } from './PostHogProvider';
